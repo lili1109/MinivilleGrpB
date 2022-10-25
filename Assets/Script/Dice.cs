45b346fba1692c006b2a2b34e9ce61d80658ff4b
Ptimouss <gaetan.tapia@gmail.com>
2022-10-25 10:21:22 +0200

    Ajout des classes
    Ajout des différentes classes Dice, Game, Pile et Player

---
+++ Assets/Script/Dice.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    		private int nbFaces;
+		private int face = 1;
+		System.Random random = new System.Random();
+
+		public Dice(int anbFaces)
+        {
+            this.nbFaces = anbFaces;
+		}
+
+		public int NbFaces
+		{
+			get { return nbFaces; }
+			set { nbFaces = value; }
+		}
+
+		public int Face
+		{
+			get { return face; }
+		}
+
+		public int Lancer()
+		{
+			face = random.Next(1, nbFaces + 1);
+			return face;
+		}
+        public override string ToString()
+        {
+			string toString = face + "";
+            return toString;
+        }
+}
