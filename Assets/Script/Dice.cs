66a8c3dc272257359862c5b2cab5e46484fbfe38
Ptimouss <gaetan.tapia@gmail.com>
2022-10-26 14:55:00 +0200

    Ajout dés + score
    Ajout de la fonctionnalité de calcul de score lors du lancé de dés.

--- Assets/Script/Dice.cs
+++ Assets/Script/Dice.cs
@@ -2,39 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Dice : MonoBehaviour
 {
 	public Sprite[] Faces;
-	private int nbFaces = 6;
+	public GameObject dé1;
+	public GameObject dé2;
 	private int face = 1;
-	Image dé;
-	System.Random random = new System.Random();
+	public int score;
+	[SerializeField] GameObject scoreTotal;
 
-    public void Start()
+    public void Throw()
     {
-		dé = GetComponent<Image>();
+        dé2.SetActive(false);
+        StartCoroutine("Shuffle");
+    }
+    public void Throw2()
+    {
+        dé2.SetActive(true);
+        StartCoroutine("Shuffle2");
     }
 
-	public void Throw(GameObject dé)
+    private IEnumerator Shuffle2()
 	{
-		dé.SetActive(false);
-		StartCoroutine("Shuffle");
-		Lancer();
-    }
-	public void Throw2(GameObject dé)
-	{
-		dé.SetActive(true);
-		StartCoroutine("Shuffle");
-		Lancer();
-	}
-	public void Lancer()
-	{
-		face = Random.Range(0, 6);
-		dé.sprite = Faces[face];
-		Debug.Log(face);
-	}
+		int randomDiceSide = 0;
+		int randomDiceSide2 = 0;
 
+		for (int i = 0; i <= 14; i++)
+		{
+			randomDiceSide = Random.Range(0, 6);
+			dé1.GetComponent<Image>().sprite = Faces[randomDiceSide];
+			randomDiceSide2 = Random.Range(0, 6);
+			dé2.GetComponent<Image>().sprite = Faces[randomDiceSide2];
+			yield return new WaitForSeconds(0.05f);
+		}
+		score = randomDiceSide + randomDiceSide2 + 2;
+		scoreTotal.GetComponent<TMP_Text>().text = score.ToString();
+	}
 	private IEnumerator Shuffle()
 	{
 		int randomDiceSide = 0;
@@ -42,13 +47,15 @@ public class Dice : MonoBehaviour
 		for (int i = 0; i <= 14; i++)
 		{
 			randomDiceSide = Random.Range(0, 6);
-			dé.sprite = Faces[randomDiceSide];
+			dé1.GetComponent<Image>().sprite = Faces[randomDiceSide];
 			yield return new WaitForSeconds(0.05f);
 		}
+		score = randomDiceSide + 1;
+		scoreTotal.GetComponent<TMP_Text>().text = score.ToString();
 	}
+
 	public override string ToString()
 	{
-
 		string toString = face + "";
 		return toString;
 	}
