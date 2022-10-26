dd3a063c5b134f53f6a6dba60c80a0df7caf9775
lisa hersant <lisa.hers11@gmail.com>
2022-10-26 11:30:37 +0200

    update Main joueur et cartes

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,16 +11,15 @@ public class Player : MonoBehaviour
     public int argent;
     [SerializeField]
     GameObject pieces;
+    [SerializeField]
+    GameObject content;
     public List<GameObject> mainJoueur;
 
     void Awake()
     {
         nbCartes = 2;
         argent = 3;
-        foreach(var carte in mainJoueur)
-        {
-            
-        }
+        AfficherMain();
        
     }
     void Start()
@@ -30,8 +29,18 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-
         pieces.GetComponent<TMP_Text>().text = argent.ToString();
+    }
+
+    void AfficherMain()
+    {
+        int k = 0;
+        foreach (var carte in mainJoueur)
+        {
+            Vector3 vect = new Vector3(170.0f * k + 50.0f, -130.0f, 0.0f);
+            Instantiate(carte, vect, Quaternion.identity, content.transform);
+            k++;
+        }
     }
    
 }
