376a3b4c8895d07dd3fcf1a4902f30ecac06c43b
lisa hersant <lisa.hers11@gmail.com>
2022-10-26 09:01:31 +0200

    maj interface pile

--- Assets/Script/Pile.cs
+++ Assets/Script/Pile.cs
@@ -5,45 +5,14 @@ using UnityEngine.UI;
 
 public class Pile : MonoBehaviour
 {
-    //public List<Cards> cartes = new List<Cards>();
-    
+
     public int nbCartes;
 
     public Pile(Cards carte)
     {
         nbCartes = 6;
         
-        /*for(int i = 0 ; i < nbCartes; i++)
-        {
-            cartes.Add(carte);
-        }*/
-        /*for (int i = 0; i < 6; i++)
-        {
-            cartes.Add(new Cards(1, "B","Immeuble 83", 1, 1));
-            cartes.Add(new Cards(1, "B", "Epicerie solidaire", 1, 2));
-            cartes.Add(new Cards(2, "V", "Musée du Papier", 2, 1));
-            cartes.Add(new Cards(3, "R", "Bock n'Roll", 1, 2));
-            cartes.Add(new Cards(4, "V", "Musée de la BD", 3, 2));
-            cartes.Add(new Cards(5, "B", "Jardin Vert", 1, 2));
-            cartes.Add(new Cards(5, "R", "RestoU", 2, 4));
-            cartes.Add(new Cards(6, "B", "Cnam Enjmin", 5, 7));
-            cartes.Add(new Cards(7, "B", "Cosmopolite", 2, 3));
-            cartes.Add(new Cards(7, "B", "CGR", 1, 3));
-            cartes.Add(new Cards(8, "V", "Patinoire", 2, 2));
-            cartes.Add(new Cards(9, "R", "Hôtel de ville", 1, 3));
-            cartes.Add(new Cards(10, "V", "Gare SNCF", 3, 4));
-            cartes.Add(new Cards(11, "B", "Bare Le Nil", 1, 2));
-            cartes.Add(new Cards(11, "R", "E.Leclerc", 2, 4));
-            cartes.Add(new Cards(12, "B", "Bord de la Charente", 4, 6));
-
-        }*/
-
-        //Debug
-       // Debug.Log(cartes.Count);
-      /*  foreach (Cards c in cartes)
-        {
-            Debug.Log(c);
-        }*/
+        
     }
 
     public override string ToString()
@@ -62,3 +31,45 @@ public class Pile : MonoBehaviour
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+/*for(int i = 0 ; i < nbCartes; i++)
+        {
+            cartes.Add(carte);
+        }*/
+/*for (int i = 0; i < 6; i++)
+{
+    cartes.Add(new Cards(1, "B","Immeuble 83", 1, 1));
+    cartes.Add(new Cards(1, "B", "Epicerie solidaire", 1, 2));
+    cartes.Add(new Cards(2, "V", "Musée du Papier", 2, 1));
+    cartes.Add(new Cards(3, "R", "Bock n'Roll", 1, 2));
+    cartes.Add(new Cards(4, "V", "Musée de la BD", 3, 2));
+    cartes.Add(new Cards(5, "B", "Jardin Vert", 1, 2));
+    cartes.Add(new Cards(5, "R", "RestoU", 2, 4));
+    cartes.Add(new Cards(6, "B", "Cnam Enjmin", 5, 7));
+    cartes.Add(new Cards(7, "B", "Cosmopolite", 2, 3));
+    cartes.Add(new Cards(7, "B", "CGR", 1, 3));
+    cartes.Add(new Cards(8, "V", "Patinoire", 2, 2));
+    cartes.Add(new Cards(9, "R", "Hôtel de ville", 1, 3));
+    cartes.Add(new Cards(10, "V", "Gare SNCF", 3, 4));
+    cartes.Add(new Cards(11, "B", "Bare Le Nil", 1, 2));
+    cartes.Add(new Cards(11, "R", "E.Leclerc", 2, 4));
+    cartes.Add(new Cards(12, "B", "Bord de la Charente", 4, 6));
+
+}*/
+
+//Debug
+// Debug.Log(cartes.Count);
+/*  foreach (Cards c in cartes)
+  {
+      Debug.Log(c);
+  }*/
