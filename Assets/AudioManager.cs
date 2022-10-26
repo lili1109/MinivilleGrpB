c48e88d7846decdc186a2be05c0a566ffc4df86f
LeoJ <43874017+26LeoJ@users.noreply.github.com>
2022-10-26 09:48:12 +0200

    Ajout AudioManager
    Ajout d'un component AudioManager pour gérer les sons du jeu.

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start ()
+    {
+        Play("AmbienceMusic");
+    }
+
+    public void Play(string name)
+    {
+        if(s == null)
+        {
+            Debug.LogWarning("Sound " + name + "not found!");
+            return;
+        }
+        else
+        {
+            Sound s = Array.Find(sounds, sound => sound.name == name);
+            s.source.Play();
+        }        
+        // Code for playing the sound to put in scripts //
+        // FindObjectOfType<AudioManager>().Play("SoundToPlay");
+    }
+}
